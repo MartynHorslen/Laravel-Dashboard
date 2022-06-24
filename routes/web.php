60d55e768fe2cf5a252896c81257be565a415fde
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-06-24 11:34:30 +0100

    Make dashboard/login as index page, modify nav & style

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
-Auth::routes(['register' => false]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//['register' => false]
+Auth::routes();
